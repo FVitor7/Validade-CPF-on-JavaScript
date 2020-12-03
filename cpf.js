08f8ea8f1e25379058f96b0a590e08a8f2fcb0cb
Fábio Vitor Coelho Silva <48036134+FVitor7@users.noreply.github.com>
2020-12-03 02:51:34 -0300

    Create cpf.js

---
+++ cpf.js
@@ -0,0 +1,36 @@
+function isValidCPF(cpf) {
+  var patt = new RegExp(/([0-9]).*?\1{10}/);
+    if (typeof cpf !== "string") return false
+    cpf = cpf.replace(/[\s.-]*/igm, '')
+    if (
+        !cpf ||
+        cpf.length != 11 ||
+        patt.test(cpf) //verify repeat number
+    ) {
+        return false
+    }
+    
+    var soma = 0
+    var resto
+    for (var i = 1; i <= 9; i++) 
+        soma = soma + parseInt(cpf.substring(i-1, i)) * (11 - i)
+    resto = (soma * 10) % 11
+    if ((resto == 10) || (resto == 11))  resto = 0
+    if (resto != parseInt(cpf.substring(9, 10)) ) return false
+    soma = 0
+    for (var i = 1; i <= 10; i++) 
+        soma = soma + parseInt(cpf.substring(i-1, i)) * (12 - i)
+    resto = (soma * 10) % 11
+    if ((resto == 10) || (resto == 11))  resto = 0
+    if (resto != parseInt(cpf.substring(10, 11) ) ) return false
+    return true
+}
+
+isValidCPF('07780015556') //True
+isValidCPF('11111111111') //False
+
+
+
+
+
+
